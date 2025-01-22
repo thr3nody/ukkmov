a079d304949ab85f4c5955a3bea8dce05bb72da8
Erine Moira <me@erine.dev>
2025-01-22 14:44:46 +0700

    Pass in the env to drizzle conf.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   dialect: 'postgresql',
   schema: './server/database/schema.ts',
-  out: './server/database/migrations'
+  out: './server/database/migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!
+  }
 })
