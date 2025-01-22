0289d85c09cc1de6af2f417b66ddd381b97b3d28
Erine Moira <me@erine.dev>
2025-01-22 14:45:34 +0700

    Schema thing.

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,7 +1,8 @@
 import { pgTable as table, pgEnum } from "drizzle-orm/pg-core";
 import * as t from "drizzle-orm/pg-core";
 
-export const rolesEnum = pgEnum("enum", ["subscriber", "author", "admin"]);
+export const rolesEnum = pgEnum("roles_enum", ["subscriber", "author", "admin"]);
+export const ratingsEnum = pgEnum("ratings_enum", ["1", "2", "3", "4", "5"]);
 
 export const users = table(
   "users",
@@ -10,36 +11,28 @@ export const users = table(
     name: t.text("name").notNull(),
     email: t.text("email").notNull(),
     password: t.text("password").notNull(),
-    role: rolesEnum().default("subscriber"),
+    role: rolesEnum().default("subscriber").notNull(),
     createdAt: t.timestamp().notNull(),
     updatedAt: t.timestamp(),
     deletedAt: t.timestamp(),
   },
   (table) => {
     return {
-      name: t.index('name_idx').on(table.email),
+      nameIndex: t.index('name_idx').on(table.name),
       emailIndex: t.uniqueIndex("email_idx").on(table.email),
     };
   }
 );
 
-export const userRatings = table("user_ratings", {
-  id: t.integer("id").primaryKey().generatedAlwaysAsIdentity(),
+export const reviews = table("reviews", {
+  id: t.integer("id").primaryKey().generatedByDefaultAsIdentity(),
   userId: t.integer("user_id").references(() => users.id),
   moviesId: t.integer("movies_id").references(() => movies.id),
-  content: t.integer("rating").notNull(),
+  rating: ratingsEnum().notNull(),
+  comment: t.text(),
   createdAt: t.timestamp().notNull(),
   updatedAt: t.timestamp(),
-});
-
-export const comments = table("comments", {
-  id: t.integer("id").primaryKey().generatedAlwaysAsIdentity(),
-  userId: t.integer("user_id").references(() => users.id),
-  moviesId: t.integer("movies_id").references(() => movies.id),
-  content: t.text("content").notNull(),
-  createdAt: t.timestamp().notNull(),
-  updatedAt: t.timestamp(),
-});
+})
 
 export const movies = table("movies", {
   id: t.integer("id").primaryKey().generatedAlwaysAsIdentity(),
@@ -53,7 +46,7 @@ export const movies = table("movies", {
   updatedAt: t.timestamp(),
 }, (table) => {
   return {
-    nameIndex: t.index('name_idx').on(table.name),
+    titleIndex: t.index('title_idx').on(table.name),
     slugIndex: t.uniqueIndex('slug_idx').on(table.name)
   }
 });
