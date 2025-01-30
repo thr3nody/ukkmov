22ec888eb371890e08473500bb92e73722014443
Erine Moira <me@erine.dev>
2025-01-30 09:13:57 +0700

    Init sidebase auth.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
 
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
@@ -13,4 +13,23 @@ export default defineNuxtConfig({
     config: {},
     viewer: true
   },
+  runtimeConfig: {
+    baseURL: '/api/auth'
+  },
+  auth: {
+    // globalAppMiddleware: true,
+    originEnvKey: 'NUXT_BASE_URL',
+    provider: {
+      type: 'local',
+      endpoints: {
+        signIn: { path: '/login', method: 'post'},
+        signUp: { path: '/register', method: 'post'},
+        signOut: { path: '/logout', method: 'post'},
+        getSession: { path: '/session', method: 'get'}
+      },
+      pages: {
+        login: 'login'
+      }
+    },
+  }
 });
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { status, data } = useAuth()
+</script>
+
+<style>
+
+</style>
