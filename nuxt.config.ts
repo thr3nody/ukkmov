05b86403faa86a972a6878106d57d28ce7270ad5
Erine Moira <me@erine.dev>
2025-02-05 07:28:12 +0700

    Modules and tailwind setup thing.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
-
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@sidebase/nuxt-auth",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/icon"
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
     configPath: 'tailwind.config',
