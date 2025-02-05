5c5f02ea5be493def55fbb123fa40a95a96562d4
Erine Moira <me@erine.dev>
2025-02-05 19:34:19 +0700

    Register page attempt, still broken.

--- components/Contain/Register.vue
+++ components/Contain/Register.vue
@@ -1,19 +1,24 @@
 <template>
   <TabsContent value="register">
     <Card>
-      <CardHeader>
-        <CardTitle>Register</CardTitle>
-        <CardDescription>Join us.</CardDescription>
-      </CardHeader>
+      <form @submit.prevent="handleSignup">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Join us.</CardDescription>
+        </CardHeader>
 
         <CardContent class="space-y-2">
           <div class="space-y-1">
+            <Label for="name">Name</Label>
+            <Input id="name" placeholder="Name" v-model="name" />
+          </div>
+          <div class="space-y-1">
             <Label for="email">Email</Label>
-            <Input id="email" placeholder="Email" />
+            <Input id="email" placeholder="Email" v-model="email" />
           </div>
           <div class="space-y-1">
             <Label for="password">Make a Password</Label>
-            <Input id="password" placeholder="P4ssw0rd" />
+            <Input id="password" placeholder="P4ssw0rd" v-model="password" />
           </div>
           <div class="space-y-1">
             <Label for="confirmPassword">Confirm Password</Label>
@@ -22,10 +27,37 @@
         </CardContent>
 
         <CardFooter>
-          <Button @submit.prevent="">
-            Login
+          <Button>
+            Register
           </Button>
         </CardFooter>
+      </form>
     </Card>
   </TabsContent>
 </template>
+
+<script setup lang="ts">
+const name = ref('')
+const email = ref('')
+const password = ref('')
+
+const { signUp } = useAuth()
+
+async function handleSignup() {
+  try {
+    await signUp(
+      {
+        name: name.value,
+        email: email.value,
+        password: password.value
+      },
+      {
+        callbackUrl: '/profile',
+        redirect: true,
+      }
+    )
+  } catch (error: any) {
+    console.error("Sign Up fail with error:", error)
+  }
+}
+</script>
