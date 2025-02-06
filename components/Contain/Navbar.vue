2bf6da217eafb262c4da8add3accb54f6650319f
Erine Moira <me@erine.dev>
2025-02-06 11:43:23 +0700

    Client only navbar.

--- components/Contain/Navbar.vue
+++ components/Contain/Navbar.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="flex justify-end">
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NuxtLink to="/movies">
-            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
-              Move Lists
-            </NavigationMenuLink>
-          </NuxtLink>
-        </NavigationMenuItem>
+    <ClientOnly>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NuxtLink to="/movies">
+              <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                Move Lists
+              </NavigationMenuLink>
+            </NuxtLink>
+          </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NuxtLink to="/auth">
-            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
-              Login
-            </NavigationMenuLink>
-          </NuxtLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+          <NavigationMenuItem v-if="!user">
+            <NuxtLink to="/auth">
+              <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                Login
+              </NavigationMenuLink>
+            </NuxtLink>
+          </NavigationMenuItem>
+
+
+          <NavigationMenuItem v-if="user">
+            <NuxtLink to="/profile">
+              <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                Profile
+              </NavigationMenuLink>
+            </NuxtLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </ClientOnly>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { navigationMenuTriggerStyle } from '../ui/navigation-menu';
+const { user } = useUserSession()
 </script>
