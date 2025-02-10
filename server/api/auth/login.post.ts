eea70c35d86cfac96f479301fa278b2a35bf985d
Erine Moira <me@erine.dev>
2025-02-10 14:20:41 +0700

    Login API works now.
    Need some tweaks.
    - Better error messages

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,3 +1,53 @@
+import { z } from "zod"
+import { compare as c } from "bcrypt"
+
+const bodySchema = z.object({
+  email: z.string().email("Please enter valid email."),
+  password: z.string().min(8, "Password must be at least 8 characters long."),
+})
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+  const { email, password } = await readValidatedBody(event, bodySchema.parse)
+
+  const users = await useDrizzle()
+    .select()
+    .from(tables.users)
+    .where(eq(tables.users.email, email))
+    .execute()
+
+  if (!users || users.length === 0) {
+    throw createError({
+      statusCode: 401,
+      message: "Bad credentials."
+    })
+  }
+
+  const user = users[0]
+
+  const isPasswordValid = await c(password, user.password);
+  if (!isPasswordValid) {
+    throw createError({
+      statusCode: 401,
+      message: "Bad credentials",
+    });
+  }
+
+  await setUserSession(event, {
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role
+    },
+  });
+
+  return {
+    success: true,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role
+    },
+  }
 })
