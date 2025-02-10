bdc60054b375be534d7d56bd0711f3ab7d50aea5
Erine Moira <me@erine.dev>
2025-02-10 14:22:05 +0700

    Register user view now also works better.

--- components/Contain/Register.vue
+++ components/Contain/Register.vue
@@ -10,23 +10,31 @@
         <CardContent class="space-y-2">
           <div class="space-y-1">
             <Label for="name">Name</Label>
-            <Input id="name" placeholder="Name" v-model="name" type="text" />
+            <Input id="name" placeholder="Name" v-model="credentials.name" type="text" />
           </div>
           <div class="space-y-1">
             <Label for="email">Email</Label>
-            <Input id="email" placeholder="Email" v-model="email" type="email" />
+            <Input id="email" placeholder="Email" v-model="credentials.email" type="email" />
           </div>
           <div class="space-y-1">
             <Label for="password">Make a Password</Label>
-            <Input id="password" placeholder="P4ssw0rd" v-model="password" type="password" />
+            <Input id="password" placeholder="P4ssw0rd" v-model="credentials.password" type="password" />
           </div>
           <div class="space-y-1">
             <Label for="confirmPassword">Confirm Password</Label>
             <Input id="confirmPassword" placeholder="Conf1rm p4ssw0rd" v-model="confirmPassword" type="password" />
           </div>
-          <div class="space-x-y">
-            <p v-if="error">{{ error }}</p>
-          </div>
+          <ClientOnly>
+            <Alert variant="destructive" v-if="passwordMismatch" class="flex space-x-2">
+              <Icon name="ion:alert-circle-outline" />
+              <div>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {{ passwordMismatch }}
+                </AlertDescription>
+              </div>
+            </Alert>
+          </ClientOnly>
         </CardContent>
 
         <CardFooter>
@@ -40,38 +48,39 @@
 </template>
 
 <script setup lang="ts">
-const name = ref('')
-const email = ref('')
-const password = ref('')
-const confirmPassword = ref("")
+const { fetch: refreshSession } = useUserSession()
+const confirmPassword = ref('')
+
+const credentials = reactive({
+  name: '',
+  email: '',
+  password: ''
+})
 
 const error = ref<String>('')
 
-// const { signUp } = useAuth()
+const passwordMismatch = computed(() => {
+  if (credentials.password && confirmPassword.value && credentials.password !== confirmPassword.value) {
+    return "Passwords don't match."
+  }
+  return ''
+})
 
 async function handleSignup() {
-  console.warn('Empty function.')
-//   error.value = ''
-  
-//   if (password.value !== confirmPassword.value) {
-//     error.value = "Password doesn't match."
-//     return
-//   }
+  try {
+    $fetch('/api/auth/register', {
+      method: 'POST',
+      body: credentials
+    })
+      .then(async () => {
+        await refreshSession()
+        await navigateTo('/profile')
+      })
+      .catch(() => {
+        createError("Register fail.")
+      })
+  } catch {
 
-//   try {
-//     await signUp(
-//       {
-//         name: name.value,
-//         email: email.value,
-//         password: password.value,
-//       },
-//       {
-//         callbackUrl: '/profile',
-//         redirect: true,
-//       }
-//     )
-//   } catch (error: any) {
-//     console.error("Sign Up fail with error:", error)
-//   }
+  }
 }
 </script>
