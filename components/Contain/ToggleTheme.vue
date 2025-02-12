08b48d36c930f3f7c853da188e5653bb4d246e61
Erine Moira <me@erine.dev>
2025-02-12 19:31:49 +0700

    Change toggle theme styling a bit.

--- components/Contain/ToggleTheme.vue
+++ components/Contain/ToggleTheme.vue
@@ -1,7 +1,7 @@
 <template>
   <DropdownMenu>
     <DropdownMenuTrigger>
-      <Button variant="link">
+      <Button variant="outline" size="icon" class="h-[40px] w-[40px]">
         <Icon name="uil:sun" class="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Icon name="uil:moon" class="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span class="sr-only">Toggle theme</span>
