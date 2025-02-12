18f2369fa7c26494789416bbaf78075a5ea69695
Erine Moira <me@erine.dev>
2025-02-12 18:42:29 +0700

    Deletion of unnecessary component.

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="flex pt-48 justify-center items-center">
-    <ViewAuth />
+    <div>
+      <Tabs default-value="login" class="w-[400px]">
+        <TabsList class="grid w-full grid-cols-2">
+          <TabsTrigger value="login">
+            Login
+          </TabsTrigger>
+
+          <TabsTrigger value="register">
+            Register
+          </TabsTrigger>
+        </TabsList>
+        
+        <ContainLogin />
+        <ContainRegister />
+      </Tabs>
+
+      <div>
+        <NuxtLink to="/" class="space-x-1 mt-1 inline-flex p-1 items-center opacity-80 rounded-sm group hover:opacity-100 transition-all duration-200">
+          <Icon name="uil:angle-left-b" size="16" />
+          <span class="text-sm group-hover:underline group-hover:underline-offset-1">Go Back</span>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
