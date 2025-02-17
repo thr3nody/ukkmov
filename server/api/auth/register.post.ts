529e6240a314f56b44ef971e2f17f69eccabb623
Erine Moira <me@erine.dev>
2025-02-17 10:20:10 +0700

    Also make it return the createdAt.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,58 +1,64 @@
-import { z } from "zod"
-import { hash as h, } from "bcrypt"
+import { z } from "zod";
+import { hash as h } from "bcrypt";
 
 const bodySchema = z.object({
   name: z.string().min(1, "Name is required."),
   email: z.string().email("Please enter valid email."),
   password: z.string().min(8, "Password must be at least 8 characters long."),
-})
+});
 
 export default defineEventHandler(async (event) => {
   try {
-    const {name, email, password} = await readValidatedBody(event, bodySchema.parse)
+    const { name, email, password } = await readValidatedBody(
+      event,
+      bodySchema.parse,
+    );
     if (!name || !email || !password) {
       throw createError({
         statusCode: 500,
-        statusMessage: "Missing credential."
-      })
+        statusMessage: "Missing credential.",
+      });
     }
 
-    const hashedPassword = await h(password, 12)
+    const hashedPassword = await h(password, 12);
 
-    const result = await useDrizzle().insert(tables.users)
+    const result = await useDrizzle()
+      .insert(tables.users)
       .values({
         name: name,
         email: email,
         password: hashedPassword,
         createdAt: new Date(),
       })
-      .returning()
+      .returning();
 
     if (!result || result.length === 0) {
       throw createError({
         statusCode: 500,
         statusMessage: "User registration failed.",
-      }); 
+      });
     }
 
-    const newUser = result[0]
+    const newUser = result[0];
     await setUserSession(event, {
       user: {
         id: newUser.id,
         name: newUser.name,
         email: newUser.email,
-        role: newUser.role
-      }
-    })
+        role: newUser.role,
+        createdAt: newUser.createdAt,
+      },
+    });
 
     return {
       success: true,
       user: {
         id: newUser.id,
-        name: newUser.name, 
+        name: newUser.name,
         email: newUser.email,
-        role: newUser.role
-      }
+        role: newUser.role,
+        createdAt: newUser.createdAt,
+      },
     };
   } catch (error: any) {
     return {
@@ -60,4 +66,4 @@ export default defineEventHandler(async (event) => {
       message: error.message || "An error occurred during registration.",
     };
   }
-})
+});
