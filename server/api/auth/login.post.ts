e8f7da36d3ce38245be3e054aea608b76b7c33d6
Erine Moira <me@erine.dev>
2025-02-17 10:19:25 +0700

    Make it return a new createdAt object.

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,28 +1,28 @@
-import { z } from "zod"
-import { compare as c } from "bcrypt"
+import { z } from "zod";
+import { compare as c } from "bcrypt";
 
 const bodySchema = z.object({
   email: z.string().email("Please enter valid email."),
   password: z.string().min(8, "Password must be at least 8 characters long."),
-})
+});
 
 export default defineEventHandler(async (event) => {
-  const { email, password } = await readValidatedBody(event, bodySchema.parse)
+  const { email, password } = await readValidatedBody(event, bodySchema.parse);
 
   const users = await useDrizzle()
     .select()
     .from(tables.users)
     .where(eq(tables.users.email, email))
-    .execute()
+    .execute();
 
   if (!users || users.length === 0) {
     throw createError({
       statusCode: 401,
-      message: "Bad credentials."
-    })
+      message: "Bad credentials.",
+    });
   }
 
-  const user = users[0]
+  const user = users[0];
 
   const isPasswordValid = await c(password, user.password);
   if (!isPasswordValid) {
@@ -37,7 +37,8 @@ export default defineEventHandler(async (event) => {
       id: user.id,
       name: user.name,
       email: user.email,
-      role: user.role
+      role: user.role,
+      createdAt: user.createdAt,
     },
   });
 
@@ -47,7 +48,8 @@ export default defineEventHandler(async (event) => {
       id: user.id,
       name: user.name,
       email: user.email,
-      role: user.role
+      role: user.role,
+      createdAt: user.createdAt,
     },
-  }
-})
+  };
+});
