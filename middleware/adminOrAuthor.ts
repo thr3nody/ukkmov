fb9e50ce519e73c991f7490619d5e33fab05f933
Erine Moira <me@erine.dev>
2025-02-17 14:46:26 +0700

    Remove unnecessary check.

--- middleware/adminOrAuthor.ts
+++ middleware/adminOrAuthor.ts
@@ -1,9 +1,5 @@
 export default defineNuxtRouteMiddleware(() => {
   const { user } = useUserSession();
-  if (!user) {
-    return navigateTo("/auth");
-  }
-
   if (!["admin", "author"].includes(user.value!.role)) {
     return navigateTo("/profile");
   }
