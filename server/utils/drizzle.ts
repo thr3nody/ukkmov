2eba6051ad80fcdc176d0e6580bcca850c6b8a05
Erine Moira <me@erine.dev>
2025-02-18 14:46:55 +0700

    Just formatting.

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,22 +1,22 @@
-import 'dotenv/config'
-import { drizzle } from 'drizzle-orm/node-postgres'
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 export { sql, eq, and, or } from "drizzle-orm";
-import pkg from "pg"
-const { Pool } = pkg
+import pkg from "pg";
+const { Pool } = pkg;
 
 import * as schema from "../database/schema";
 
 export const tables = schema;
-const connectionString = process.env.DATABASE_URL!
+const connectionString = process.env.DATABASE_URL!;
 if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined in the environment variable.")
+  throw new Error("DATABASE_URL is not defined in the environment variable.");
 }
 
 const pool = new Pool({
-  connectionString
-})
+  connectionString,
+});
 
 export function useDrizzle() {
-  return drizzle(pool, {schema})
+  return drizzle(pool, { schema });
 }
 export type User = typeof schema.users.$inferSelect;
