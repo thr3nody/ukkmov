5304f57e87f8676b4e503e9d34508695fcb6a263
Erine Moira <me@erine.dev>
2025-02-18 14:42:08 +0700

    Finalized very basic sidebar that needs improvements.

--- components/Contain/Sidebar.vue
+++ components/Contain/Sidebar.vue
@@ -1,8 +1,10 @@
 <template>
   <Sidebar collapsible="offcanvas">
     <SidebarHeader>
-      <NuxtLink to="/"
-        class="flex items-center gap-2 p-2 bg-[#16A34A] hover:bg-[#199848] dark:bg-[#22C55E] rounded-md dark:hover:bg-[#20B356] text-sm justify-center text-[#FFF1F2] dark:text-[#052E16] transition-all duration-200">
+      <NuxtLink
+        to="/"
+        class="flex items-center gap-2 p-2 bg-[#16A34A] hover:bg-[#199848] dark:bg-[#22C55E] rounded-md dark:hover:bg-[#20B356] text-sm justify-center text-[#FFF1F2] dark:text-[#052E16] transition-all duration-200"
+      >
         <Icon name="mdi:home" size="24" />
         <span> Go Back to Main Page </span>
       </NuxtLink>
@@ -42,6 +44,7 @@
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarGroup>
+
       <SidebarGroup v-if="user!.role === 'admin'">
         <SidebarGroupLabel>Administration</SidebarGroupLabel>
         <SidebarGroupContent>
@@ -66,7 +69,13 @@
         </Avatar>
         <div class="flex gap-1">
           <ContainToggleTheme />
-          <Button type="button" variant="destructive" size="icon" @click="handleLogout" class="w-[40px] h-[40px]">
+          <Button
+            type="button"
+            variant="destructive"
+            size="icon"
+            @click="handleLogout"
+            class="w-[40px] h-[40px]"
+          >
             <Icon name="mdi:logout" />
           </Button>
         </div>
