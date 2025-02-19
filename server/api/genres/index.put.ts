b54600c5b22420d6bad48728efaf812ff73b54ba
Erine Moira <me@erine.dev>
2025-02-19 07:53:57 +0700

    Ok this PUT now good.

--- server/api/genres/index.put.ts
+++ server/api/genres/index.put.ts
@@ -1,11 +1,33 @@
 import { z } from "zod";
 
 const bodySchema = z.object({
-  id: z.coerce.number().min(1, "Number can't be empty."),
+  id: z.coerce.number().min(1, "Number cannot be empty."),
+  name: z.string().min(1, "Name cannot be empty."),
 });
 
 export default defineEventHandler(async (event) => {
   try {
-    const dataToDelete = await readValidatedBody(event, bodySchema.parse);
+    const { id, name } = await readValidatedBody(event, bodySchema.parse);
+
+    const [updatedRecord] = await useDrizzle()
+      .update(tables.genres)
+      .set({ name })
+      .where(eq(tables.genres.id, id))
+      .returning({
+        updatedID: tables.genres.id,
+        updateName: tables.genres.name,
+      });
+
+    if (!updatedRecord) {
+      return {
+        success: false,
+        message: `No genre found with ID ${id}.`,
+      };
+    }
+
+    return {
+      success: true,
+      updated: updatedRecord,
+    };
   } catch (error: any) {}
 });
