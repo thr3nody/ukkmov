92620b81af11f80812d1bc8fe6e5bcc320e14cbf
Erine Moira <me@erine.dev>
2025-02-19 07:40:54 +0700

    Change icon package.

--- components/Contain/ToggleTheme.vue
+++ components/Contain/ToggleTheme.vue
@@ -2,8 +2,10 @@
   <DropdownMenu>
     <DropdownMenuTrigger>
       <Button variant="outline" size="icon" class="h-[40px] w-[40px]">
-        <Icon name="uil:sun" class="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Icon name="uil:moon" class="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Icon name="mdi:white-balance-sunny"
+          class="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Icon name="mdi:moon-and-stars"
+          class="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span class="sr-only">Toggle theme</span>
       </Button>
     </DropdownMenuTrigger>
@@ -22,5 +24,5 @@
 </template>
 
 <script lang="ts" setup>
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 </script>
