2fece7bb41d317dd2899fbe566c015a85cd5a0ab
Erine Moira <me@erine.dev>
2025-03-06 02:08:30 +0700

    Just basic user session data fetch and logout function.

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    
+    <p v-if="user">Hello {{ user.name }}</p>
+    <p v-if="user">You are {{ user.role }}</p>
+    <p v-if="user">and {{ user }}</p>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script lang="ts" setup>
+definePageMeta({
+  middleware: ["authenticated"],
+});
 
+const { user, clear: clearSession } = useUserSession();
+
+async function logout() {
+  await clearSession();
+  await navigateTo("/");
+}
 </script>
-
-<style>
-
-</style>
