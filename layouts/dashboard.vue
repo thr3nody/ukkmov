229aa59a4e5527409e33caaf28852ed8b036abd0
Erine Moira <me@erine.dev>
2025-03-08 21:18:00 +0700

    Styling for the dashboar main component length.

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -2,10 +2,25 @@
   <div>
     <SidebarProvider>
       <ContainSidebar />
-      <main>
+      <main :style="mainStyle" class="flex-1 px-4 py-2">
         <SidebarTrigger />
         <slot />
       </main>
     </SidebarProvider>
   </div>
 </template>
+
+<script setup lang="ts">
+const sidebar = ref<HTMLElement | null>(null);
+const sidebarWidth = ref(0);
+
+onMounted(() => {
+  if (sidebar.value) {
+    sidebarWidth.value = sidebar.value.offsetWidth;
+  }
+});
+
+const mainStyle = computed(() => ({
+  minWidth: `calc(100% - $(sidebarWidth.value)px)`,
+}));
+</script>
