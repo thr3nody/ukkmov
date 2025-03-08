1fe0de8b562f5993525a585753a4cb74e2a0e365
Erine Moira <me@erine.dev>
2025-03-08 22:07:45 +0700

    Add casts only data dashboard.
    + Main table
    + Filter actions
    + useCastsTable composable

---
+++ components/Contain/Dash/Casts/Actions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex gap-2 items-center py-4">
+    <Input class="max-w-sm" placeholder="Filter casts..."
+      :modelValue="props.table.getColumn('name')?.getFilterValue() as string" @update:modelValue="(value: any) => table.getColumn('name')?.setFilterValue(value)
+        " />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Table } from "@tanstack/vue-table";
+
+const props = defineProps<{
+  table: Table<any>;
+}>();
+</script>
---
+++ components/Contain/Dash/Casts/Table.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="w-full">
+    <ContainDashCastsActions :table="table" />
+
+    <div class="rounded-md border">
+      <Table>
+        <TableCaption>Casts (Only) Data</TableCaption>
+        <TableHeader>
+          <TableRow
+            v-for="headerGroup in table.getHeaderGroups()"
+            :key="headerGroup.id"
+          >
+            <TableHead
+              v-for="header in headerGroup.headers"
+              :key="header.id"
+              :data-pinned="header.column.getIsPinned()"
+              :class="
+                cn(
+                  { 'sticky bg-background/95': header.column.getIsPinned() },
+                  header.column.getIsPinned() === 'left' ? 'left-0' : 'right-0',
+                )
+              "
+            >
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <template v-if="table.getRowModel().rows?.length">
+            <template v-for="row in table.getRowModel().rows" :key="row.id">
+              <TableRow :data-state="row.getIsSelected() && 'selected'">
+                <TableCell
+                  v-for="cell in row.getVisibleCells()"
+                  :key="cell.id"
+                  :data-pinned="cell.column.getIsPinned()"
+                  :class="
+                    cn(
+                      { 'sticky bg-background/95': cell.column.getIsPinned() },
+                      cell.column.getIsPinned() === 'left'
+                        ? 'left-0'
+                        : 'right-0',
+                    )
+                  "
+                >
+                  <FlexRender
+                    :render="cell.column.columnDef.cell"
+                    :props="cell.getContext()"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow v-if="row.getIsExpanded()">
+                <TableCell :colspan="row.getAllCells().length">
+                  <pre>{{ JSON.stringify(row.original, null, 2) }}</pre>
+                </TableCell>
+              </TableRow>
+            </template>
+          </template>
+          <TableRow v-else>
+            <TableCell :colspan="columns.length" class="h-24 text-center">
+              No results/data.
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+
+    <div class="flex items-center justify-end space-x-2 py-4">
+      <div class="flex-1 text-sm text-muted-foreground">
+        {{ table.getFilteredSelectedRowModel().rows.length }} of
+        {{ table.getFilteredRowModel().rows.length }} row(s) selected.
+      </div>
+      <div class="space-x-2">
+        <Button
+          variant="outline"
+          size="sm"
+          :disabled="!table.getCanPreviousPage()"
+          @click="table.previousPage()"
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          :disabled="!table.getCanNextPage()"
+          @click="table.nextPage()"
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FlexRender } from "@tanstack/vue-table";
+import { cn } from "@/lib/utils";
+import { useCastsTable } from "~/composables/useCastsTable";
+
+const casts = ref<Casts[]>([]);
+
+async function loadCasts() {
+  const response = await $fetch<{ success: boolean; casts: Casts[] }>(
+    "/api/casts",
+  );
+  casts.value = response.casts;
+  console.log(casts.value);
+}
+
+onMounted(() => {
+  loadCasts();
+});
+
+const { table, columns } = useCastsTable(casts);
+</script>
