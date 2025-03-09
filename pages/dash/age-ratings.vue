73daaf37641fe2b964a62179712092cddaeea933
Erine Moira <me@erine.dev>
2025-03-10 00:28:09 +0700

    Implement dash ratings table to the page.

---
+++ pages/dash/age-ratings.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full">
+    <ContainDashAgeRatingsTable />
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "dashboard",
+  middleware: ["authenticated", "admin-or-author"],
+});
+</script>
