3ee2bc07484935669166a5cebf9941fe285feb30
Erine Moira <me@erine.dev>
2025-03-09 20:22:32 +0700

    Add get api for casts.

--- server/api/casts/index.get.ts
+++ server/api/casts/index.get.ts
@@ -1,10 +1,17 @@
 export default defineEventHandler(async (event) => {
   try {
-    return await useDrizzle().select().from(tables.casts);
+    const data = await useDrizzle().select().from(tables.casts);
+    return {
+      success: true,
+      casts: data.map((castsData) => ({
+        id: castsData.id,
+        name: castsData.name,
+      })),
+    };
   } catch (error: any) {
     return createError({
       statusCode: 404,
-      message: "Can't find any genres.",
+      message: "Can't find any casts.",
     });
   }
 });
