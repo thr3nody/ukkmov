5a199740d0829eb89e55c6865b88e74cd002b96e
Erine Moira <me@erine.dev>
2025-03-10 00:27:16 +0700

    Age ratings data table.

---
+++ components/Contain/Dash/AgeRatings/Actions.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex gap-2 items-center py-4">
+    <Input
+      class="max-w-sm"
+      placeholder="Filter age ratings..."
+      :modelValue="props.table.getColumn('content')?.getFilterValue() as string"
+      @update:modelValue="
+        (value: any) => table.getColumn('content')?.setFilterValue(value)
+      "
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Table } from "@tanstack/vue-table";
+
+const props = defineProps<{
+  table: Table<any>;
+}>();
+</script>
