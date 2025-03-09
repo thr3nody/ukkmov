6cc76f0fef54cafa22690aa9e4ec866cb2466c9b
Erine Moira <me@erine.dev>
2025-03-09 19:37:53 +0700

    Implement data table of users in the dashboard.

--- pages/dash/users.vue
+++ pages/dash/users.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    Users
+  <div class="w-full">
+    <ContainDashUsersTable />
   </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
-  layout: 'dashboard',
-  middleware: ['authenticated', 'admin']
-})
+  layout: "dashboard",
+  middleware: ["authenticated", "admin"],
+});
 </script>
