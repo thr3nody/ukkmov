0387a73cc79d8e30ace3c6b7ccd319f0e0c4e737
Erine Moira <me@erine.dev>
2025-03-11 17:19:07 +0700

    Movies data type add genres, age rating, and casts.

--- types/movies.d.ts
+++ types/movies.d.ts
@@ -1,9 +1,12 @@
 interface Movies {
   id: number;
   title: string;
-  slug: string;
+  slug?: string;
   synopsis: string;
   duration: number;
-  averageRating: number;
+  averageRating?: number;
   releaseDate: string;
+  genres?: Array<{ id: number; name: string }>;
+  casts?: Array<{ id: number; name: string }>;
+  ageRating?: { id: number; content: string };
 }
