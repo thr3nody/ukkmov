b85a30688f4519dbf9fa8387d27650cbbed2874b
Erine Moira <me@erine.dev>
2025-03-14 20:31:18 +0700

    Deleting movie also deletes the poster from /public/posters/ now

--- server/api/movies/index.delete.ts
+++ server/api/movies/index.delete.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
+import { promises as fs } from "fs";
+import path from "path";
 
 const bodySchema = z.object({
   id: z.coerce.number().min(1, "Number can't be empty."),
@@ -7,15 +9,37 @@ const bodySchema = z.object({
 export default defineEventHandler(async (event) => {
   try {
     const { id } = await readValidatedBody(event, bodySchema.parse);
+    const [movie] = await useDrizzle()
+      .select({
+        posterPath: tables.movies.posterPath,
+      })
+      .from(tables.movies)
+      .where(eq(tables.movies.id, id));
+
+    if (!movie) {
+      return {
+        success: false,
+        message: "Movie not found.",
+      };
+    }
 
     const deletedData = await useDrizzle()
       .delete(tables.movies)
       .where(eq(tables.movies.id, id))
       .returning({ deletedId: tables.movies.id });
 
+    if (deletedData.length && movie.posterPath) {
+      const filePath = path.resolve("./public/posters", movie.posterPath);
+      try {
+        await fs.unlink(filePath);
+      } catch (err) {
+        console.warn("Poster file not found or already deleted:", err);
+      }
+    }
+
     return {
       success: true,
-      deltedGenresId: deletedData,
+      deltedGenresId: deletedData[0],
     };
   } catch (error: any) {
     return {
