878686bcdd231d807e03f2560fb5439a62f4f016
Erine Moira <me@erine.dev>
2025-03-17 00:48:53 +0700

    Manage reviews and manage age ratings button.

--- components/Contain/Sidebar.vue
+++ components/Contain/Sidebar.vue
@@ -1,10 +1,8 @@
 <template>
   <Sidebar collapsible="offcanvas">
     <SidebarHeader>
-      <NuxtLink
-        to="/"
-        class="flex items-center gap-2 p-2 bg-[#16A34A] hover:bg-[#199848] dark:bg-[#22C55E] rounded-md dark:hover:bg-[#20B356] text-sm justify-center text-[#FFF1F2] dark:text-[#052E16] transition-all duration-200"
-      >
+      <NuxtLink to="/"
+        class="flex items-center gap-2 p-2 bg-[#16A34A] hover:bg-[#199848] dark:bg-[#22C55E] rounded-md dark:hover:bg-[#20B356] text-sm justify-center text-[#FFF1F2] dark:text-[#052E16] transition-all duration-200">
         <Icon name="mdi:home" size="24" />
         <span> Go Back to Main Page </span>
       </NuxtLink>
@@ -16,11 +14,7 @@
         <SidebarGroupContent>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton
-                asChild
-                variant="default"
-                class="sidebar-button"
-              >
+              <SidebarMenuButton asChild variant="default" class="sidebar-button">
                 <NuxtLink to="/dash/movies">
                   <Icon name="mdi:movie-outline" size="24" />
                   <span>Manage Movies</span>
@@ -29,11 +23,7 @@
             </SidebarMenuItem>
 
             <SidebarMenuItem>
-              <SidebarMenuButton
-                asChild
-                variant="default"
-                class="sidebar-button"
-              >
+              <SidebarMenuButton asChild variant="default" class="sidebar-button">
                 <NuxtLink to="/dash/genres">
                   <Icon name="mdi:category-outline" size="24" />
                   <span>Manage Genres</span>
@@ -42,14 +32,28 @@
             </SidebarMenuItem>
 
             <SidebarMenuItem>
-              <SidebarMenuButton
-                asChild
-                variant="default"
-                class="sidebar-button"
-              >
+              <SidebarMenuButton asChild variant="default" class="sidebar-button">
                 <NuxtLink to="/dash/casts">
-                  <Icon name="mdi:people" size="24" />
+                  <Icon name="mdi:people-outline" size="24" />
                   <span>Manage Casts</span>
+                </NuxtLink>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+                <NuxtLink to="/dash/reviews">
+                  <Icon name="mdi:comment-text-multiple-outline" size="24" />
+                  <span>Manage Reviews</span>
+                </NuxtLink>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+                <NuxtLink to="/dash/age-ratings">
+                  <Icon name="mdi:number-9-plus-circle-outline" size="24" />
+                  <span>Manage Age Ratings</span>
                 </NuxtLink>
               </SidebarMenuButton>
             </SidebarMenuItem>
@@ -62,11 +66,7 @@
         <SidebarGroupContent>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton
-                asChild
-                variant="default"
-                class="sidebar-button"
-              >
+              <SidebarMenuButton asChild variant="default" class="sidebar-button">
                 <NuxtLink to="/dash/users">
                   <Icon name="mdi:user-card-details-outline" size="24" />
                   <span>Manage Users</span>
@@ -85,13 +85,7 @@
         </Avatar>
         <div class="flex gap-1">
           <ContainToggleTheme />
-          <Button
-            type="button"
-            variant="destructive"
-            size="icon"
-            @click="handleLogout"
-            class="w-[40px] h-[40px]"
-          >
+          <Button type="button" variant="destructive" size="icon" @click="handleLogout" class="w-[40px] h-[40px]">
             <Icon name="mdi:logout" />
           </Button>
         </div>
