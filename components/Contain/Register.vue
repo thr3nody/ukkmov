2a9af5f7bf55556e9da8ba18c9a0ec2ee453d174
Erine Moira <me@erine.dev>
2025-03-18 11:50:19 +0700

    Better error message in the register component.

--- components/Contain/Register.vue
+++ components/Contain/Register.vue
@@ -10,22 +10,46 @@
         <CardContent class="space-y-2">
           <div class="space-y-1">
             <Label for="name">Name</Label>
-            <Input id="name" placeholder="Name" v-model="credentials.name" type="text" />
+            <Input
+              id="name"
+              placeholder="Name"
+              v-model="credentials.name"
+              type="text"
+            />
           </div>
           <div class="space-y-1">
             <Label for="email">Email</Label>
-            <Input id="email" placeholder="Email" v-model="credentials.email" type="email" />
+            <Input
+              id="email"
+              placeholder="Email"
+              v-model="credentials.email"
+              type="email"
+            />
           </div>
           <div class="space-y-1">
             <Label for="password">Make a Password</Label>
-            <Input id="password" placeholder="P4ssw0rd" v-model="credentials.password" type="password" />
+            <Input
+              id="password"
+              placeholder="P4ssw0rd"
+              v-model="credentials.password"
+              type="password"
+            />
           </div>
           <div class="space-y-1">
             <Label for="confirmPassword">Confirm Password</Label>
-            <Input id="confirmPassword" placeholder="Conf1rm p4ssw0rd" v-model="confirmPassword" type="password" />
+            <Input
+              id="confirmPassword"
+              placeholder="Conf1rm p4ssw0rd"
+              v-model="confirmPassword"
+              type="password"
+            />
           </div>
           <ClientOnly>
-            <Alert variant="destructive" v-if="passwordMismatch" class="flex space-x-2">
+            <Alert
+              variant="destructive"
+              v-if="passwordMismatch"
+              class="flex space-x-2"
+            >
               <Icon name="ion:alert-circle-outline" />
               <div>
                 <AlertTitle>Error</AlertTitle>
@@ -37,10 +61,9 @@
           </ClientOnly>
         </CardContent>
 
-        <CardFooter>
-          <Button>
-            Register
-          </Button>
+        <CardFooter class="space-x-1">
+          <Button>Register</Button>
+          <div v-if="message" class="text-red-500">{{ message }}</div>
         </CardFooter>
       </form>
     </Card>
@@ -48,39 +71,49 @@
 </template>
 
 <script setup lang="ts">
-const { fetch: refreshSession } = useUserSession()
-const confirmPassword = ref('')
+const { fetch: refreshSession } = useUserSession();
+const confirmPassword = ref("");
 
 const credentials = reactive({
-  name: '',
-  email: '',
-  password: ''
-})
+  name: "",
+  email: "",
+  password: "",
+});
 
-const error = ref<String>('')
+const message = ref<String | undefined>("");
 
 const passwordMismatch = computed(() => {
-  if (credentials.password && confirmPassword.value && credentials.password !== confirmPassword.value) {
-    return "Passwords don't match."
+  if (
+    credentials.password &&
+    confirmPassword.value &&
+    credentials.password !== confirmPassword.value
+  ) {
+    return "Passwords don't match.";
   }
-  return ''
-})
+  return "";
+});
 
 async function handleSignup() {
   try {
-    $fetch('/api/auth/register', {
-      method: 'POST',
-      body: credentials
-    })
-      .then(async () => {
-        await refreshSession()
-        await navigateTo('/profile')
-      })
-      .catch(() => {
-        createError("Register fail.")
-      })
-  } catch {
+    if (!credentials.name || !credentials.email || !credentials.password) {
+      message.value = "Fill all required fields to register.";
+    }
+    const response = await $fetch<{ success: boolean; message?: string }>(
+      "/api/auth/register",
+      {
+        method: "POST",
+        body: credentials,
+      },
+    );
 
+    if (response.success) {
+      await refreshSession();
+      await navigateTo("/profile");
+    }
+  } catch (error: any) {
+    console.log("Error registering:", error);
+    message.value =
+      error?.data?.message || error.message || "An error occurred.";
   }
 }
 </script>
