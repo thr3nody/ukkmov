278ee03777386026b8b4b241224b892847b262b0
Erine Moira <me@erine.dev>
2025-03-18 11:49:51 +0700

    Better error messages in the login component.

--- components/Contain/Login.vue
+++ components/Contain/Login.vue
@@ -10,18 +10,27 @@
         <CardContent class="space-y-2">
           <div class="space-y-1">
             <Label for="email">Email</Label>
-            <Input id="email" placeholder="Email" v-model="credentials.email" type="email" />
+            <Input
+              id="email"
+              placeholder="Email"
+              v-model="credentials.email"
+              type="email"
+            />
           </div>
           <div class="space-y-1">
             <Label for="password">Password</Label>
-            <Input id="password" placeholder="P4ssw0rd" v-model="credentials.password" type="password" />
+            <Input
+              id="password"
+              placeholder="P4ssw0rd"
+              v-model="credentials.password"
+              type="password"
+            />
           </div>
         </CardContent>
 
-        <CardFooter>
-          <Button type="submit">
-            Login
-          </Button>
+        <CardFooter class="space-x-1">
+          <Button type="submit"> Login </Button>
+          <div v-if="message" class="text-red-500">{{ message }}</div>
         </CardFooter>
       </Card>
     </TabsContent>
@@ -29,28 +38,40 @@
 </template>
 
 <script setup lang="ts">
-const { fetch: refreshSession } = useUserSession()
+const { fetch: refreshSession } = useUserSession();
 
 const credentials = reactive({
-  email: '',
-  password: ''
-})
+  email: "",
+  password: "",
+});
+
+const message = ref<string | undefined>("");
 
 async function handleLogin() {
   try {
-    $fetch('/api/auth/login', {
-      method: 'POST',
-      body: credentials
-    })
-      .then(async () => {
-        await refreshSession()
-        await navigateTo('/profile')
-      })
-      .catch(() => {
-        createError("Login fail.")
-      })
-  } catch {
+    if (!credentials.email || !credentials.password) {
+      message.value = "Fill all the required field to login.";
+      return;
+    }
 
+    const response = await $fetch<{ success: boolean; message?: string }>(
+      "/api/auth/login",
+      {
+        method: "POST",
+        body: credentials,
+      },
+    );
+
+    if (response.success) {
+      await refreshSession();
+      await navigateTo("/profile");
+    } else {
+      message.value = response.message || "Login failed.";
+    }
+  } catch (error: any) {
+    console.log("Error logging in:", error);
+    message.value =
+      error?.data?.message || error.message || "An error occurred.";
   }
 }
 </script>
