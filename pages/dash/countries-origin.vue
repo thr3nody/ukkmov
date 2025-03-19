1600137bc161a7872c6c5dab10cccadec58a6084
Erine Moira <me@erine.dev>
2025-03-19 21:13:48 +0700

    Add the countries origin dashboard page.

---
+++ pages/dash/countries-origin.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full">
+    <ContainDashCountriesOriginTable />
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "dashboard",
+  middleware: ["authenticated", "admin-or-author"],
+});
+</script>
