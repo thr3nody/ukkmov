cf2b7866bd08af5bdabc0a28db8ffe8703303156
Erine Moira <me@erine.dev>
2025-03-20 08:53:07 +0700

    Add 'if user is authenticated only' component.

--- pages/movies/[slug].vue
+++ pages/movies/[slug].vue
@@ -1,13 +1,8 @@
 <template>
   <div class="max-w-screen-lg mx-auto py-8 px-4 space-y-8">
     <div class="mb-4">
-      <iframe
-        v-if="movieDetail?.trailerLink"
-        class="aspect-video w-full h-full rounded-md"
-        :src="movieDetail?.trailerLink"
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
+      <iframe v-if="movieDetail?.trailerLink" class="aspect-video w-full h-full rounded-md"
+        :src="movieDetail?.trailerLink" frameborder="0" allowfullscreen></iframe>
       <div v-else class="flex items-center justify-center">
         <p>No Trailer Available</p>
       </div>
@@ -16,61 +11,42 @@
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
       <div class="lg:col-span-2">
         <h2 class="text-xl w-full font-semibold mb-4">Reviews</h2>
-        <div class="mb-4 p-4 border rounded-md">
+        <div v-if="user" class="mb-4 p-4 border rounded-md">
           <h3 class="text-lg font-bold mb-2">Your Review</h3>
           <div class="mb-2">
             <div class="flex items-center space-x-1">
-              <span
-                v-for="star in 5"
-                :key="star"
-                class="cursor-pointer"
-                @click="setRating(star)"
-              >
-                <Icon
-                  :name="star <= userRating! ? 'mdi:star' : 'mdi:star-outline'"
-                  class="text-yellow-500 h-6 w-6"
-                />
+              <span v-for="star in 5" :key="star" class="cursor-pointer" @click="setRating(star)">
+                <Icon :name="star <= userRating! ? 'mdi:star' : 'mdi:star-outline'" class="text-yellow-500 h-6 w-6" />
               </span>
             </div>
           </div>
           <div class="mb-2">
-            <label class="block text-sm font-medium text-gray-700"
-              >Comment</label
-            >
-            <Textarea
-              rows="3"
-              v-model="userComment"
-              class="border rounded w-full p-1"
-            ></Textarea>
+            <label class="block text-sm font-medium text-gray-700">Comment</label>
+            <Textarea rows="3" v-model="userComment" class="border rounded w-full p-1"></Textarea>
           </div>
           <Button variant="outline" class="mt-2" @click="submitReview">
             Submit
           </Button>
-          <p
-            v-if="reviewMessage"
-            class="mt-2 text-sm"
-            :class="reviewSuccess ? 'text-green-500' : 'text-red-500'"
-          >
+          <p v-if="reviewMessage" class="mt-2 text-sm" :class="reviewSuccess ? 'text-green-500' : 'text-red-500'">
             {{ reviewMessage }}
+          </p>
+        </div>
+        <div v-else class="mb-4 p-4 border rounded-md flex items-center">
+          <p>
+            <NuxtLink to="/auth"><Button variant="default">Login</Button></NuxtLink>
+            to review
           </p>
         </div>
 
         <div v-if="validReviews && validReviews.length">
-          <Card
-            v-for="(review, index) in movieDetail?.reviews"
-            :key="index"
-            class="mb-4 p-4 rounded-md"
-          >
+          <Card v-for="(review, index) in movieDetail?.reviews" :key="index" class="mb-4 p-4 rounded-md">
             <CardTitle class="flex items-center justify-between">
               <div class="inline-flex space-x-2 items-center">
                 <Avatar>
-                  <AvatarImage
-                    :src="
-                      review.user?.avatarPath
-                        ? `/avatars/${review.user.avatarPath}`
-                        : ''
-                    "
-                  />
+                  <AvatarImage :src="review.user?.avatarPath
+                      ? `/avatars/${review.user.avatarPath}`
+                      : ''
+                    " />
                   <AvatarFallback>PFP</AvatarFallback>
                 </Avatar>
                 <p class="font-semibold">
@@ -95,12 +71,8 @@
         <h2 class="text-xl w-full font-semibold mb-4">Detail</h2>
         <Card class="p-2">
           <CardTitle class="mb-2">
-            <img
-              v-if="movieDetail?.posterPath"
-              :src="`/posters/${movieDetail?.posterPath}`"
-              :alt="`Poster for ${movieDetail!.title}`"
-              class="h-auto object-cover rounded"
-            />
+            <img v-if="movieDetail?.posterPath" :src="`/posters/${movieDetail?.posterPath}`"
+              :alt="`Poster for ${movieDetail!.title}`" class="h-auto object-cover rounded" />
           </CardTitle>
 
           <div class="space-y-2">
@@ -166,7 +138,7 @@ const userComment = ref<string>("");
 const reviewMessage = ref<string>("");
 const reviewSuccess = ref<boolean>(false);
 
-const currentUserId = user.value!.id;
+const currentUserId = user.value?.id;
 
 const fetchMovieDetail = async () => {
   try {
@@ -228,40 +200,41 @@ async function submitReview() {
     return;
   }
 
-  try {
-    const response = await $fetch<{
-      success: boolean;
-      message?: string;
-    }>("/api/reviews", {
-      method: "POST",
-      body: {
-        userId: currentUserId,
-        movieId: movieDetail.value?.id,
-        rating: userRating.value,
-        comment: userComment.value,
-      },
-    });
+  if (currentUserId)
+    try {
+      const response = await $fetch<{
+        success: boolean;
+        message?: string;
+      }>("/api/reviews", {
+        method: "POST",
+        body: {
+          userId: currentUserId,
+          movieId: movieDetail.value?.id,
+          rating: userRating.value,
+          comment: userComment.value,
+        },
+      });
 
-    if (response.success) {
-      reviewMessage.value = "Review saved successfully!";
-      reviewSuccess.value = true;
-      userRating.value = null;
-      userComment.value = "";
-      hideMessageAfterDelay(500);
+      if (response.success) {
+        reviewMessage.value = "Review saved successfully!";
+        reviewSuccess.value = true;
+        userRating.value = null;
+        userComment.value = "";
+        hideMessageAfterDelay(500);
 
-      fetchMovieDetail();
-    } else {
-      reviewMessage.value = response.message || "Failed to save review.";
+        fetchMovieDetail();
+      } else {
+        reviewMessage.value = response.message || "Failed to save review.";
+        reviewSuccess.value = false;
+        hideMessageAfterDelay(5000);
+      }
+    } catch (err: any) {
+      reviewMessage.value = err.message || "An error occurred while saving.";
       reviewSuccess.value = false;
-      hideMessageAfterDelay(5000);
-    }
-  } catch (err: any) {
-    reviewMessage.value = err.message || "An error occurred while saving.";
-    reviewSuccess.value = false;
-    console.error("Error upserting review:", err);
+      console.error("Error upserting review:", err);
 
-    hideMessageAfterDelay(3000);
-  }
+      hideMessageAfterDelay(3000);
+    }
 }
 
 function hideMessageAfterDelay(delay: number) {
