829eea89e10e36eeb607199df250d45c7394dc1a
Erine Moira <me@erine.dev>
2025-03-20 08:51:40 +0700

    Add per user reviews page.

---
+++ pages/dash/per-user-reviews.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ContainDashPerUserReviewsTable />
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "dashboard",
+  middleware: ["authenticated", "admin-or-author"],
+});
+</script>
