afb45997b8be7a417f572b10bbbf6b346de589c9
Erine Moira <me@erine.dev>
2025-03-20 08:52:28 +0700

    Implement per user reviews table in the front end.

---
+++ components/Contain/Dash/PerUserReviews/Table.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="w-full">
+    <!-- <ContainDashGenresActions :table="table" @refreshGenres="refreshGenres" /> -->
+
+    <div class="rounded-md border">
+      <Table>
+        <TableCaption>Per User Reviews Report</TableCaption>
+        <TableHeader>
+          <TableRow
+            v-for="headerGroup in table.getHeaderGroups()"
+            :key="headerGroup.id"
+          >
+            <TableHead
+              v-for="header in headerGroup.headers"
+              :key="header.id"
+              :data-pinned="header.column.getIsPinned()"
+              v-bind="$attrs"
+              :class="
+                cn(
+                  { 'sticky bg-background/95': header.column.getIsPinned() },
+                  header.column.getIsPinned() === 'left' ? 'left-0' : 'right-0',
+                )
+              "
+            >
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <template v-if="table.getRowModel().rows?.length">
+            <template v-for="row in table.getRowModel().rows" :key="row.id">
+              <TableRow :data-state="row.getIsSelected() && 'selected'">
+                <TableCell
+                  v-for="cell in row.getVisibleCells()"
+                  :key="cell.id"
+                  :data-pinned="cell.column.getIsPinned()"
+                  :class="
+                    cn(
+                      { 'sticky bg-background/95': cell.column.getIsPinned() },
+                      cell.column.getIsPinned() === 'left'
+                        ? 'left-0'
+                        : 'right-0',
+                    )
+                  "
+                >
+                  <FlexRender
+                    :render="cell.column.columnDef.cell"
+                    :props="cell.getContext()"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow v-if="row.getIsExpanded()">
+                <TableCell :colspan="row.getAllCells().length">
+                  <pre>{{ JSON.stringify(row.original, null, 2) }}</pre>
+                </TableCell>
+              </TableRow>
+            </template>
+          </template>
+          <TableRow v-else>
+            <TableCell :colspan="columns.length" class="h-24 text-center">
+              No results/data.
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+
+    <div class="flex items-center justify-end space-x-2 py-4">
+      <div class="flex-1 text-sm text-muted-foreground">
+        {{ table.getFilteredSelectedRowModel().rows.length }} of
+        {{ table.getFilteredRowModel().rows.length }} row(s) selected.
+      </div>
+      <div class="space-x-2">
+        <Button
+          variant="outline"
+          size="sm"
+          :disabled="!table.getCanPreviousPage()"
+          @click="table.previousPage()"
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          :disabled="!table.getCanNextPage()"
+          @click="table.nextPage()"
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FlexRender } from "@tanstack/vue-table";
+import { cn } from "@/lib/utils";
+import { usePerUserReviewsTable } from "~/composables/usePerUserReviewsTable";
+
+const perUserReviews = ref<PerUserReviews[]>([]);
+
+async function loadPerUserReviews() {
+  const response = await $fetch<{
+    success: boolean;
+    perUserReviews: PerUserReviews[];
+  }>("/api/per-user-reviews");
+  perUserReviews.value = response.perUserReviews;
+  console.log(perUserReviews.value);
+}
+
+onMounted(() => {
+  loadPerUserReviews();
+});
+
+const { table, columns } = usePerUserReviewsTable(perUserReviews);
+
+// function refreshPerUserReviews() {
+//   loadPerUserReviews();
+// }
+</script>
