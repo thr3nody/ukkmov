8479a348152ce53ace59f24b1ebb4e7c9a1796cf
Erine Moira <me@erine.dev>
2025-03-20 08:52:13 +0700

    Mainly adding the button to per user reviews dashboard.

--- components/Contain/Sidebar.vue
+++ components/Contain/Sidebar.vue
@@ -1,8 +1,10 @@
 <template>
   <Sidebar collapsible="offcanvas">
     <SidebarHeader>
-      <NuxtLink to="/"
-        class="flex items-center gap-2 p-2 bg-[#16A34A] hover:bg-[#199848] dark:bg-[#22C55E] rounded-md dark:hover:bg-[#20B356] text-sm justify-center text-[#FFF1F2] dark:text-[#052E16] transition-all duration-200">
+      <NuxtLink
+        to="/"
+        class="flex items-center gap-2 p-2 bg-[#16A34A] hover:bg-[#199848] dark:bg-[#22C55E] rounded-md dark:hover:bg-[#20B356] text-sm justify-center text-[#FFF1F2] dark:text-[#052E16] transition-all duration-200"
+      >
         <Icon name="mdi:home" size="24" />
         <span> Go Back to Main Page </span>
       </NuxtLink>
@@ -10,11 +12,35 @@
 
     <SidebarContent>
       <SidebarGroup>
+        <SidebarGroupLabel>Reports</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
+                <NuxtLink to="/dash/per-user-reviews">
+                  <Icon name="mdi:comment-account-outline" size="24" />
+                  <span>Per User Reviews</span>
+                </NuxtLink>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+
+      <SidebarGroup>
         <SidebarGroupLabel>Author Dashboard</SidebarGroupLabel>
         <SidebarGroupContent>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
                 <NuxtLink to="/dash/movies">
                   <Icon name="mdi:movie-outline" size="24" />
                   <span>Manage Movies</span>
@@ -23,7 +49,11 @@
             </SidebarMenuItem>
 
             <SidebarMenuItem>
-              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
                 <NuxtLink to="/dash/genres">
                   <Icon name="mdi:category-outline" size="24" />
                   <span>Manage Genres</span>
@@ -32,7 +62,11 @@
             </SidebarMenuItem>
 
             <SidebarMenuItem>
-              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
                 <NuxtLink to="/dash/casts">
                   <Icon name="mdi:people-outline" size="24" />
                   <span>Manage Casts</span>
@@ -41,7 +75,11 @@
             </SidebarMenuItem>
 
             <SidebarMenuItem>
-              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
                 <NuxtLink to="/dash/reviews">
                   <Icon name="mdi:comment-text-multiple-outline" size="24" />
                   <span>Manage Reviews</span>
@@ -50,10 +88,27 @@
             </SidebarMenuItem>
 
             <SidebarMenuItem>
-              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
                 <NuxtLink to="/dash/age-ratings">
                   <Icon name="mdi:number-9-plus-circle-outline" size="24" />
                   <span>Manage Age Ratings</span>
+                </NuxtLink>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
+                <NuxtLink to="/dash/countries-origin">
+                  <Icon name="mdi:map-outline" size="24" />
+                  <span>Manage Countries Origin</span>
                 </NuxtLink>
               </SidebarMenuButton>
             </SidebarMenuItem>
@@ -66,7 +121,11 @@
         <SidebarGroupContent>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton asChild variant="default" class="sidebar-button">
+              <SidebarMenuButton
+                asChild
+                variant="default"
+                class="sidebar-button"
+              >
                 <NuxtLink to="/dash/users">
                   <Icon name="mdi:user-card-details-outline" size="24" />
                   <span>Manage Users</span>
@@ -77,20 +136,6 @@
         </SidebarGroupContent>
       </SidebarGroup>
     </SidebarContent>
-
-    <SidebarFooter>
-      <div class="flex items-center justify-between">
-        <Avatar>
-          <!-- TODO: Avatar for users, make it possible. -->
-        </Avatar>
-        <div class="flex gap-1">
-          <ContainToggleTheme />
-          <Button type="button" variant="destructive" size="icon" @click="handleLogout" class="w-[40px] h-[40px]">
-            <Icon name="mdi:logout" />
-          </Button>
-        </div>
-      </div>
-    </SidebarFooter>
   </Sidebar>
 </template>
 
